4a8a64f041c1c1101fcdf6131907643ac08e9573
Pat Wilson <zerostride@gmail.com>
2010-03-24 21:31:30 -0700

    Added start of DustSharp.

---
+++ Effecter.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace GameClay
+{
+	namespace Dust
+	{		
+		/// <summary>
+		/// Manipulates the particles in a Simulation to create localized effects.
+		/// </summary>
+		///
+		/// <remarks>
+		/// An Effecter is an object which manipulates the particles in a Simulation.
+		/// These effecters can be used to create almost limitless customizable particle movement
+		/// which, when combined with shared simulations and emitter-specific representations,
+		/// can create dazzling effects. Each Effecter attached to a Simulation adds
+		/// at least an additional iteration over the SimulationData each frame.
+		/// </remarks>
+		public interface Effecter
+		{
+			/// <summary> Assigns the Simulation this Effecter will manipulate</summary>
+			bool Simulation { get; set; }
+			
+			/// <summary>
+			/// An object-to-world transform for this Effecter.
+			/// </summary>
+			float Transform { get; set; } // TODO: Matrix not float
+			
+			/// <summary>
+			/// Advances the Effecter physics by a number of seconds.
+			/// </summary>
+         	///
+         	/// <param name="dt">The time which has passed since the last call to advanceTime(), in seconds.</param>
+			void advanceTime(float dt);
+		}
+	}
+}
