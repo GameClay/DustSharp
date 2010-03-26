78a99d1449b6004ed04174bde96dcbf35a5d2ad1
Pat Wilson <zerostride@gmail.com>
2010-03-25 22:18:59 -0700

    Added basic test runner framework.

--- DustBuster/Main.cs
+++ DustBuster/Main.cs
@@ -1,12 +1,45 @@
 using System;
+using System.Collections.Generic;
 
-namespace DustBuster
+namespace GameClay
 {
-	class MainClass
+	namespace DustBuster
 	{
-		public static void Main (string[] args)
+		class MainClass
 		{
-			Console.WriteLine ("Hello World!");
+			public static int Main (string[] args)
+			{
+				// This is pretty braindead, but it works
+				List<ITest> testList = new List<ITest>();
+				
+				// Add some tests
+				
+				// Run all the tests
+				Console.WriteLine ("DustBuster running " + testList.Count + " automated tests.");
+				int testsPassed = 0;
+				int testsFailed = 0;
+				
+				foreach (ITest test in testList)
+				{
+					switch (test.RunTest ())
+					{
+					case Result.Passed:
+						testsPassed++;
+						break;
+						
+					case Result.Failed:
+						testsFailed++;
+						break;
+					}
+				}
+				
+				// Report to the console (kind of lame)
+				Console.WriteLine ("   " + testsPassed + " tests passed.");
+				Console.WriteLine ("   " + testsFailed + " tests failed.");
+				
+				// Return 0 if no tests failed
+				return testsFailed;
+			}
 		}
 	}
 }
