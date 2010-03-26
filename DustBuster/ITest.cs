01285f775608b30e08fb12956ccaa116839ddb56
Pat Wilson <zerostride@gmail.com>
2010-03-25 22:09:42 -0700

    Added start of a simple test suite.

---
+++ DustBuster/ITest.cs
@@ -0,0 +1,31 @@
+
+using System;
+
+namespace GameClay
+{
+	namespace DustBuster
+	{
+	
+		/// <summary>
+		/// Test result
+		/// </summary>
+		public enum Result
+		{
+			Passed,
+			Failed
+		}
+		
+		/// <summary>
+		/// This is a really simple automated test.
+		/// </summary>
+		public interface ITest
+		{
+			/// <summary>
+			/// Run the test, and return the result of the test.
+			/// </summary>
+			/// 
+			/// <returns> Returns the <see cref="Result"/> of the test. </returns>
+			Result RunTest();
+		}
+	}
+}
---
+++ DustBuster/Main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DustBuster
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
